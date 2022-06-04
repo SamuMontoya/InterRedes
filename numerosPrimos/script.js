dc3a915f38693bd8d80b2888686397811d9e5b2e
samu <samuel7montoya@gmail.com>
2022-06-04 17:34:07 -0500

    FunciónPrimos - Array

--- numerosPrimos/script.js
+++ numerosPrimos/script.js
@@ -1,19 +1,21 @@
-const num = 8
 let primo = true
-
-for(i=2; i< num/2; i++){
-    if(num%i === 0){
+let primos = (num) => {
+    for(i=2; i<num/2; i++){
+        if(num%i === 0){
+            primo = false
+        }
+    }if(num === 4 || num === 1){
         primo = false
     }
-}
-if(primo){
-    console.log("primo")
-}else{
-    console.log("no primo")
+    if(primo){
+        console.log("primo")
+    }else{
+        console.log("no primo")
+    }
 }
 
 let array = []
-for(i=0; i<=100; i++){
+for(i=1;i<=10;i++){
     array.push(i)
 }
 console.log(array)
